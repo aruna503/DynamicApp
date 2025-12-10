9f850798b913083f2b09f5bf7c77940070c820ac
aruna503 <44997651+aruna503@users.noreply.github.com>
2025-12-10 21:46:43 +0530

    Update DynamicIcon.js

--- www/DynamicIcon.js
+++ www/DynamicIcon.js
@@ -1,5 +1,15 @@
 var exec = require('cordova/exec');
 
-exports.changeIcon = function (iconName, success, error) {
-    exec(success, error, "DynamicIcon", "changeIcon", [iconName]);
+var DynamicIcon = {
+    changeIcon: function(iconName, successCallback, errorCallback) {
+        exec(
+            successCallback,
+            errorCallback,
+            "DynamicIcon",      // must match the <feature name="DynamicIcon">
+            "changeIcon",       // must match the action in Java
+            [iconName]
+        );
+    }
 };
+
+module.exports = DynamicIcon;
