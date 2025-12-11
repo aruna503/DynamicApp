ba72417a5642f2091201c01dcdb9fbe67ebc20c7
aruna503 <44997651+aruna503@users.noreply.github.com>
2025-12-11 11:44:28 +0530

    Create DynamicIcon.java

---
+++ cordova-plugin-dynamicappicon/src/android/com/outsystems/dynamicappicon/DynamicIcon.java
@@ -0,0 +1,63 @@
+package com.outsystems.dynamicappicon;
+ 
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.content.ComponentName;
+ 
+import org.apache.cordova.CallbackContext;
+import org.apache.cordova.CordovaPlugin;
+ 
+import org.json.JSONArray;
+ 
+public class DynamicIcon extends CordovaPlugin {
+ 
+    @Override
+    public boolean execute(String action, JSONArray args, CallbackContext callbackContext) {
+        if ("changeIcon".equals(action)) {
+            String icon = args.optString(0);
+            switchIcon(icon, callbackContext);
+            return true;
+        }
+        return false;
+    }
+ 
+    private void switchIcon(String icon, CallbackContext callback) {
+        Context context = cordova.getActivity().getApplicationContext();
+        PackageManager pm = context.getPackageManager();
+ 
+        String pkg = context.getPackageName();
+ 
+        // The alias names MUST match plugin.xml
+        String normalAlias  = pkg + ".IconNormal";
+        String premiumAlias = pkg + ".IconPremium";
+        String privateAlias = pkg + ".IconPrivate";
+ 
+        // Disable all
+        setState(pm, normalAlias, false);
+        setState(pm, premiumAlias, false);
+        setState(pm, privateAlias, false);
+ 
+        // Enable selected icon
+        if ("premium".equalsIgnoreCase(icon)) {
+            setState(pm, premiumAlias, true);
+        } else if ("private".equalsIgnoreCase(icon)) {
+            setState(pm, privateAlias, true);
+        } else {
+            setState(pm, normalAlias, true);
+        }
+ 
+        callback.success("Icon changed to: " + icon);
+    }
+ 
+    private void setState(PackageManager pm, String alias, boolean enabled) {
+        ComponentName cn = new ComponentName(alias.substring(0, alias.lastIndexOf(".")), alias);
+        pm.setComponentEnabledSetting(
+            cn,
+            enabled ?
+                    PackageManager.COMPONENT_ENABLED_STATE_ENABLED :
+                    PackageManager.COMPONENT_ENABLED_STATE_DISABLED,
+            PackageManager.DONT_KILL_APP
+        );
+    }
+}
+ 
